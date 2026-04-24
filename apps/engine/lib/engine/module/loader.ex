AdamGoode/expert

commit a666c59c61bfce00fdc127384ee451780765d887

fix: improve startup time and module introspection (#604)
Elixir 1.19 introduced lazy loading of modules, we suspect this is
causing issues on larger projects during compilation, where we'd see
messages like "Module.foo/2 is undefined, Module does not exist. Did you
mean Module.foo/2?".

This change loads asks our module loader to load all unloaded modules
during builds.

Additionally, this change bumps elixir_sense, which was calling
Code.ensure_compiled on every module during startup. On larger projects
this would mean that completion would not be available for around 30-40
seconds.

apps/engine/lib/engine/module/loader.ex
@@ -17,6 +17,25 @@ defmodule Engine.Module.Loader do
     Agent.start_link(initialize, name: __MODULE__)
   end
 
+  def load_all(module_list) do
+    Agent.update(__MODULE__, fn modules ->
+      loaded_modules =
+        case Code.ensure_all_loaded(module_list) do
+          :ok ->
+            modules
+
+          {:error, errors} ->
+            failed_modules = MapSet.new(errors, &elem(&1, 0))
+            Enum.reject(module_list, &MapSet.member?(failed_modules, &1))
+        end
+
+      newly_loaded =
+        Map.new(loaded_modules, fn module_name -> {module_name, {:module, module_name}} end)
+
+      Map.merge(modules, newly_loaded)
+    end)
+  end
+
   def ensure_loaded(module_name) do
     Agent.get_and_update(__MODULE__, fn
       %{^module_name => result} = state ->

apps/engine/lib/engine/build/project.ex
@@ -1,6 +1,7 @@
 defmodule Engine.Build.Project do
   alias Engine.Build
   alias Engine.Build.Isolation
+  alias Engine.Module.Loader
   alias Engine.Plugin
   alias Engine.Progress
   alias Forge.Internet
@@ -62,6 +63,7 @@ defmodule Engine.Build.Project do
       Mix.Task.clear()
       Progress.report(token, message: "Compiling #{Project.display_name(project)}")
       result = compile_in_isolation()
+      maybe_load_modules()
       Project.ensure_hex_and_rebar()
       Mix.Task.run(:loadpaths)
       result
@@ -84,6 +86,18 @@ defmodule Engine.Build.Project do
         )
 
         Build.Error.refine_diagnostics(diagnostics)
+    end
+  end
+
+  defp maybe_load_modules do
+    if Elixir.Features.lazy_loading?() do
+      modules_to_load =
+        for {mod, _, false} <- :code.all_available() do
+          List.to_atom(mod)
+        end
+
+      Logger.info("Loading #{length(modules_to_load)} modules")
+      Loader.load_all(modules_to_load)
     end
   end
 

apps/engine/mix.exs
@@ -49,7 +49,7 @@ defmodule Engine.MixProject do
       Mix.Credo.dependency(),
       Mix.Dialyzer.dependency(),
       {:elixir_sense,
-       github: "elixir-lsp/elixir_sense", ref: "e3ddc403554050221a2fd19a10a896fa7525bc02"},
+       github: "elixir-lsp/elixir_sense", ref: "fb9ee0e68db8b46fbff4d7a78ee4bb7f63db0669"},
       {:forge, path: "../forge"},
       {:gen_lsp, "~> 0.11.3"},
       {:patch, "~> 0.15", only: [:dev, :test], runtime: false},

apps/forge/lib/elixir/features.ex
@@ -25,6 +25,10 @@ defmodule Elixir.Features do
     Versions.current_elixir_matches?(">= 1.17.0")
   end
 
+  def lazy_loading? do
+    Versions.current_elixir_matches?(">= 1.19.0")
+  end
+
   @doc """
   Whether the `:compressed` ETS table option can be safely used.
 

commit 7429476cddf251cf50b4e0e0f35ee3fef783c6b7

fix: eager loading for large projects (#610)
On a fairly large project as of today I'm seeing the following error
causing the application compiling failure:

```
22:50:58.662 instance_id=19DBC1BD7FD [debug] Node port message: 
22:50:58.622 [error] GenServer XPEngine.Build terminating
** (stop) exited in: GenServer.call(XPEngine.Module.Loader, {:update, #Function<1.33268632/1 in XPEngine.Module.Loader.load_all/1>}, 5000)
    ** (EXIT) time out
    (elixir 1.19.4) lib/gen_server.ex:1142: GenServer.call/3
    (xp_engine 0.1.0) lib/engine/build/project.ex:66: anonymous fn/2 in XPEngine.Build.Project.do_compile/3
    (xp_engine 0.1.0) lib/engine/build/project.ex:72: XPEngine.Build.Project.do_compile/3
    (xp_engine 0.1.0) lib/engine/build/project.ex:21: anonymous fn/3 in XPEngine.Build.Project.compile/2
    (xp_engine 0.1.0) lib/engine/progress.ex:6: XPEngine.Progress.execute_work/2
    (mix 1.19.4) lib/mix/project.ex:557: Mix.Project.in_project/4
    (elixir 1.19.4) lib/file.ex:2013: File.cd!/2
    (xp_engine 0.1.0) lib/engine/mix.ex:31: anonymous fn/3 in XPEngine.Mix.in_project/2
Last message: :timeout
State: %XPEngine.Build.State{project: %XPForge.Project{root_uri: "file:///Users/pawel.sw/jump/Jump/api", mix_exs_uri: "file:///Users/pawel.sw/jump/Jump/api/mix.exs", kind: :mix, mix_env: nil, mix_target: nil, env_variables: %{}, project_module: Jump.MixProject, entropy: 22236}, build_number: 0, uri_to_document: %{}, project_compile: :force, last_deps_fetch_result: nil}
```

This seems to be caused by #604 and the fact that it tries to load all
modules inside `Agent.update` with a default timeout of 5 seconds. This
is not enough time, so it crashes.

The solution here is to perform the loading outside `Agent.update` and
only update the agent when we have the results.

apps/engine/lib/engine/module/loader.ex
@@ -18,22 +18,20 @@ defmodule Engine.Module.Loader do
   end
 
   def load_all(module_list) do
-    Agent.update(__MODULE__, fn modules ->
-      loaded_modules =
-        case Code.ensure_all_loaded(module_list) do
-          :ok ->
-            modules
+    loaded_modules =
+      case Code.ensure_all_loaded(module_list) do
+        :ok ->
+          module_list
 
-          {:error, errors} ->
-            failed_modules = MapSet.new(errors, &elem(&1, 0))
-            Enum.reject(module_list, &MapSet.member?(failed_modules, &1))
-        end
+        {:error, errors} ->
+          failed_modules = MapSet.new(errors, &elem(&1, 0))
+          Enum.reject(module_list, &MapSet.member?(failed_modules, &1))
+      end
 
-      newly_loaded =
-        Map.new(loaded_modules, fn module_name -> {module_name, {:module, module_name}} end)
+    newly_loaded =
+      Map.new(loaded_modules, fn module_name -> {module_name, {:module, module_name}} end)
 
-      Map.merge(modules, newly_loaded)
-    end)
+    Agent.update(__MODULE__, fn modules -> Map.merge(modules, newly_loaded) end)
   end
 
   def ensure_loaded(module_name) do